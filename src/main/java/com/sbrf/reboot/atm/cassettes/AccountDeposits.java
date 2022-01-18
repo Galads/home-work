a819b0bedc9703f428109b844a66d288de7fd7d0
Galads <nikola_2212@mail.ru>
2022-01-18 15:59:48 +0300

    add hw-5 with tests + bonus

---
+++ src/main/java/com/sbrf/reboot/atm/cassettes/AccountDeposits.java
@@ -0,0 +1,36 @@
+package com.sbrf.reboot.atm.cassettes;
+
+import lombok.NonNull;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountDeposits<T extends Deposit> {
+    List<T> arrDeposits;
+
+    AccountDeposits(ArrayList<T> accountDeposit) {
+        this.arrDeposits = accountDeposit;
+    }
+
+    public double getApy(@NonNull T deposit) {
+        return deposit.getApy();
+    }
+
+    public ArrayList<T> getAmountAfter() {
+        ArrayList<T> apyResAmount = new ArrayList<>();
+
+        arrDeposits.forEach(e -> {
+                    BigDecimal befVal = e.getValue();
+                        BigDecimal resVal = befVal.multiply(
+                                BigDecimal.valueOf(e.getApy()),
+                                MathContext.DECIMAL64);
+                        resVal = resVal.divide(BigDecimal.valueOf(100.0), MathContext.DECIMAL64);
+                        e.setValue(befVal.add(resVal));
+                    apyResAmount.add(e);
+                }
+        );
+        return apyResAmount;
+    }
+}
---
+++ src/main/java/com/sbrf/reboot/atm/cassettes/Banknote.java
@@ -0,0 +1,13 @@
+package com.sbrf.reboot.atm.cassettes;
+
+public class Banknote {
+    private int denomination;
+
+    public int getDenomination() {
+        return denomination;
+    }
+
+    public void setDenomination(int newValue) {
+        this.denomination = newValue;
+    }
+}
