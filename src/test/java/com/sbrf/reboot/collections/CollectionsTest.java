8b8c9d178760645807cb60b2db620ddb024cb9e4
Galads <nikola_2212@mail.ru>
2022-01-19 02:25:40 +0300

    add hw-6 with tests + bonus

--- src/test/java/com/sbrf/reboot/collections/CollectionsTest.java
+++ src/test/java/com/sbrf/reboot/collections/CollectionsTest.java
@@ -1,8 +1,12 @@
 package com.sbrf.reboot.collections;
 
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,9 +33,12 @@ public class CollectionsTest {
     @Test
     public void addStudentToRating() {
 
-        List<String> students = null;
-
-        //...
+        List<String> students = new ArrayList<String>() {{
+            add("Козлов");
+            add("Иванов");
+            add("Петров");
+            add("Сидоров");
+        }};
 
         assertEquals(4, students.size());
     }
@@ -48,10 +55,9 @@ public class CollectionsTest {
      */
     @Test
     public void addMoneyToBox() {
+        Set<Integer> moneyBox;
 
-        List<Integer> moneyBox = null;
-
-        //...
+        moneyBox = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
         assertEquals(10, moneyBox.size());
     }
@@ -68,15 +74,38 @@ public class CollectionsTest {
      */
     @Test
     public void addBookToShelf() {
+        @RequiredArgsConstructor
         class Book {
+            @Setter
+            @Getter
+            @NonNull
+            private String name;
         }
 
-        List<Book> bookshelf = null;
-
-        //...
+        HashMap<Long, Book> bookshelf = new HashMap<Long, Book>() {{
+            put(1L, new Book("Математика"));
+            put(2L, new Book("Преступление и наказание"));
+            put(3L, new Book("Физика"));
+        }};
 
         assertEquals(3, bookshelf.size());
     }
 
+    /*
+     * Задача.
+     * Имеется список уникальных директорий в виде строк.
+     * Для уменьшения времени получения каждой конкретной директории при уходе в глубину
+     * Вы хотите записывать ее в структуру данных (кэш), чтобы в следующий раз получать директории из него.
+     *
+     * Вопрос.
+     * Какую коллекцию из Collections framework вы предпочтете использовать для хранения директорий в виде строк.
+     *
+     * Проинициализируйте cacheDirectory, добавьте в нее 3 директории что бы тест завершился успешно.
+     */
+    @Test
+    public void addPathToCache() {
+        LinkedHashSet<String> cacheDirectory = new LinkedHashSet<>(Arrays.asList("/path/1", "/path/2", "/path/3"));
 
+        assertEquals(3, cacheDirectory.size());
+    }
 }
