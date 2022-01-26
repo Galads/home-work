16f18187f887ad8f2491a5ad0da735bacd76348a
Galads <nikola_2212@mail.ru>
2022-01-27 01:03:05 +0300

    added correct deletion variants + bonus

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
+import java.util.Iterator;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveElementWithoutErrorsTest {
@@ -20,18 +22,61 @@ public class RemoveElementWithoutErrorsTest {
         }};
 
         assertThrows(ConcurrentModificationException.class, () -> {
-            
+
             for (Integer integer : list) {
                 list.remove(1);
             }
-            
         });
-
     }
 
     @Test
     public void successRemoveElementWithoutErrors() {
+        List<Integer> list = new ArrayList() {{
+            add(1);
+            add(2);
+            add(3);
+        }};
 
+        assertDoesNotThrow(() -> {
+            list.removeIf(e -> e == 1);
+        });
     }
 
+    @Test
+    public void successRemoveElementWithoutErrorSecond() {
+        List<Integer> list = new ArrayList() {{
+            add(1);
+            add(2);
+            add(3);
+        }};
+
+
+        assertDoesNotThrow(() -> {
+                    for (Iterator<Integer> it = list.iterator(); it.hasNext(); ) {
+                        Integer value = it.next();
+                        if (value == 1)
+                            it.remove();
+                    }
+                }
+        );
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrorBadVersion() {
+        List<Integer> list = new ArrayList() {{
+            add(1);
+            add(2);
+            add(3);
+        }};
+        List<Integer> rmList = new ArrayList<>();
+
+        assertDoesNotThrow(() -> {
+            for (Integer e : list) {
+                if (e == 1) {
+                    rmList.add(e);
+                }
+            }
+            list.removeAll(rmList);
+        });
+    }
 }
