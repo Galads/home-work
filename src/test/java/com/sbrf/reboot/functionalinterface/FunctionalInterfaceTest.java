4e5224c8ead669ddfdf0ce20117fa29ca7f2c3a5
Galads <nikola_2212@mail.ru>
2022-02-04 01:53:57 +0300

    added implementation + bonus

--- src/test/java/com/sbrf/reboot/functionalinterface/FunctionalInterfaceTest.java
+++ src/test/java/com/sbrf/reboot/functionalinterface/FunctionalInterfaceTest.java
@@ -1,16 +1,17 @@
 package com.sbrf.reboot.functionalinterface;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import com.sbrf.reboot.utils.JSONUtils;
+import lombok.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class FunctionalInterfaceTest {
 
@@ -26,29 +27,37 @@ public class FunctionalInterfaceTest {
         String applyAsJson(T t) throws JsonProcessingException;
     }
 
+    @FunctionalInterface
+    interface ParseReferencesParameters<T> {
+        LinkedList<String> parseAsString(T t);
+    }
+
     static class ListConverter<T> {
-        public List<String> toJsonsList(@NonNull List<T> someObjects, ObjectToJsonFunction<T> objectToJsonFunction) {
+
+        public List<String> toJsonsList(
+                @NonNull List<T> someObjects,
+                ObjectToJsonFunction<T> objectToJsonFunction) throws JsonProcessingException {
+
             List<String> result = new ArrayList<>();
             if (someObjects.isEmpty())
                 throw new IllegalArgumentException("The list is empty");
-
             //add code here...
-
+            for (T someObject : someObjects) {
+                result.add(objectToJsonFunction.applyAsJson(someObject));
+            }
             return result;
         }
     }
 
     @Test
-    public void successCallFunctionalInterface() {
+    public void successCallFunctionalInterface() throws JsonProcessingException {
         ListConverter<SomeObject> ListConverter = new ListConverter<>();
 
-        ObjectToJsonFunction<SomeObject> objectToJsonFunction = someObject -> {
-            //add code here...
-            return null;
-        };
+        //add code here...
+        ObjectToJsonFunction<SomeObject> objectToJsonFunction = JSONUtils::toJSON;
 
         List<String> strings = ListConverter.toJsonsList(
-                Arrays.asList(
+                asList(
                         new SomeObject("Object-1"),
                         new SomeObject("Object-2")
                 ),
@@ -59,4 +68,41 @@ public class FunctionalInterfaceTest {
         Assertions.assertTrue(strings.contains("{\"objectName\":\"Object-2\"}"));
     }
 
+    static class ParseParameters<T> {
+        public List<String> toListParam(
+                @NonNull List<T> objects,
+                ParseReferencesParameters<T> parseReferencesParameters) {
+            List<String> res = new ArrayList<>();
+
+            if (objects.isEmpty())
+                throw new IllegalArgumentException("The list is empty");
+            objects.forEach(e -> res.addAll(parseReferencesParameters.parseAsString(e)));
+            return res;
+        }
+    }
+
+    @Test
+    public void successCallFunctionalInterfaceSec() {
+        ParseReferencesParameters<SomeObject> parseReferencesParameters = e -> {
+            val splits = e.objectName.split("[?]")[1];
+            val arr = splits.split("[&,]");
+
+            return new LinkedList<>(Arrays.asList(arr));
+        };
+
+        ParseParameters<SomeObject> parseParameters = new ParseParameters<>();
+
+        List<String> strings = parseParameters.toListParam(
+                asList(
+                        new SomeObject("http://localhost:443?values=firstValue,secondValue,thirdValue"),
+                        new SomeObject("http://localhost:8080?values=firstValue&values=secondValue&values=thirdValue")
+                ),
+                parseReferencesParameters
+        );
+
+        Assertions.assertEquals(6, strings.size());
+        Assertions.assertEquals("values=firstValue", strings.get(0));
+        Assertions.assertEquals("thirdValue", strings.get(2));
+        Assertions.assertEquals("values=thirdValue", strings.get(5));
+    }
 }
