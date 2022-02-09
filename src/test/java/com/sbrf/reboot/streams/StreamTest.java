64d7ba3c1c731039df1b2a110e03d3f260f88aea
Galads <nikola_2212@mail.ru>
2022-02-09 07:46:45 +0300

    add impl. + bonus

--- src/test/java/com/sbrf/reboot/streams/StreamTest.java
+++ src/test/java/com/sbrf/reboot/streams/StreamTest.java
@@ -2,12 +2,17 @@ package com.sbrf.reboot.streams;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StreamTest {
@@ -21,7 +26,7 @@ public class StreamTest {
 
         List<Integer> expectedIntegers = Arrays.asList(3, 6, 8, 9);
 
-        List<Integer> actualIntegers = null; //add code here
+        List<Integer> actualIntegers = integers.stream().sorted().collect(Collectors.toList());
 
         assertEquals(expectedIntegers, actualIntegers);
     }
@@ -36,7 +41,7 @@ public class StreamTest {
 
         List<Integer> expectedIntegers = Arrays.asList(6, 8);
 
-        List<Integer> actualIntegers = null; //add code here
+        List<Integer> actualIntegers = integers.stream().filter(e -> e % 2 == 0).collect(Collectors.toList());
 
         assertEquals(expectedIntegers, actualIntegers);
 
@@ -71,10 +76,12 @@ public class StreamTest {
 
         );
 
-        List<Book> actualBooks = null; //add code here
+        List<Book> actualBooks = books.stream()
+                .filter(e -> "Maria".equals(e.author))
+                .sorted(Comparator.comparing(e -> e.price))
+                .collect(Collectors.toList());
 
         assertEquals(expectedBooks, actualBooks);
-
     }
 
     /*
@@ -88,10 +95,38 @@ public class StreamTest {
 
         List<String> expectedContracts = Arrays.asList("M-NCC-1-CH", "M-NCC-2-US", "M-NCC-3-NH");
 
-        List<String> actualContracts = null; //add code here
+        List<String> actualContracts = contracts.stream()
+                .map(e -> "M-" + e)
+                .collect(Collectors.toList());
 
         assertEquals(expectedContracts, actualContracts);
-
     }
 
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    @Getter
+    class Card {
+        private String number;
+        private List<String> operations;
+    }
+
+    @Test
+    void getAllOperations() {
+        List<Card> cardholder = Arrays.asList(
+                new Card("0408565612120001", Arrays.asList("send cash: 8$", "withdrawing cash: 200$")),
+                new Card("1111111111110000", Arrays.asList("depositing cash: 500$")),
+                new Card("1234567890000000", Arrays.asList("send cash: 2$"))
+        );
+
+        List<String> expectedOperations = Arrays.asList(
+                "send cash: 8$", "withdrawing cash: 200$",
+                "depositing cash: 500$", "send cash: 2$"
+        );
+        List<String> history = cardholder.stream()
+                .map(Card::getOperations)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        assertArrayEquals(expectedOperations.toArray(), history.toArray());
+    }
 }
