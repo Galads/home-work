2de51cf4376762c32fa542f5262322ea36111666
Galads <nikola_2212@mail.ru>
2022-02-11 05:15:27 +0300

    add implementations + bonus

--- src/main/java/com/sbrf/reboot/repository/CustomerRepository.java
+++ src/main/java/com/sbrf/reboot/repository/CustomerRepository.java
@@ -3,12 +3,16 @@ package com.sbrf.reboot.repository;
 import com.sbrf.reboot.dto.Customer;
 import lombok.NonNull;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface CustomerRepository {
 
-    boolean createCustomer(@NonNull String userName, String eMail);
+    boolean createCustomer(@NonNull String userName, String eMail) throws SQLException;
 
-    List<Customer> getAll();
+    List<Customer> getAll() throws SQLException;
 
+    boolean isExist(@NonNull String name);
+
+    boolean deleteWhere(@NonNull String name, @NonNull String eMail);
 }
--- src/test/java/com/sbrf/reboot/repository/impl/CustomerH2RepositoryTest.java
+++ src/test/java/com/sbrf/reboot/repository/impl/CustomerH2RepositoryTest.java
@@ -5,8 +5,10 @@ import com.sbrf.reboot.repository.CustomerRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CustomerH2RepositoryTest {
@@ -14,12 +16,12 @@ class CustomerH2RepositoryTest {
     private static CustomerRepository customerRepository;
 
     @BeforeAll
-    public static void before() {
+    public static void before() throws SQLException {
         customerRepository = new CustomerH2Repository();
     }
 
     @Test
-    void getAll() {
+    void getAll() throws SQLException {
         boolean tomCreated = customerRepository.createCustomer("Tom", "tom@ya.ru");
 
         List<Customer> all = customerRepository.getAll();
@@ -28,10 +30,29 @@ class CustomerH2RepositoryTest {
     }
 
     @Test
-    void createCustomer() {
+    void createCustomer() throws SQLException {
 
         boolean mariaCreated = customerRepository.createCustomer("Maria", "maria98@ya.ru");
 
         assertTrue(mariaCreated);
+    }
+
+    @Test
+    void existCustomer() throws SQLException {
+        boolean userCreated = customerRepository.createCustomer("GALADS", "galads98@ya.ru");
+
+        boolean exist1 = customerRepository.isExist("Galads");
+        boolean exist2 = customerRepository.isExist("GALADS");
+
+        assertFalse(exist1);
+        assertTrue(exist2);
+    }
+
+    @Test
+    void deleteCustomer() throws SQLException {
+        boolean userCreated1 = customerRepository.createCustomer("Lin", "lin@ya.ru");
+        boolean userCreated2 = customerRepository.createCustomer("Lin", "lin@ya.ru");
+
+        customerRepository.deleteWhere("Lin", "lin@ya.ru");
     }
 }
